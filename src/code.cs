712deea79007db44ff8147d2cd2cdae0385059d6
joranvar <bart.post@gmail.com>
2015-12-29 06:22:12 +0100

    Initialize parameters from constructor
    Now, we can solve different problems, just by calling the constructor
    with different values.

--- src/code.cs
+++ src/code.cs
@@ -15,13 +15,22 @@ class Value
 
 class Solver
 {
-    const int N = 8;
-    static int S = 2;
-    static int T = 3;
-    static int[] numbers = new int[N] { 1, 6, 8, 7, 6, 2, 1, 8 };
-    static List<Value> values = new List<Value>();
-    static int maxPoint = -1;
-    static List<Value> result;
+    int N;
+    int S;
+    int T;
+    int[] numbers;
+
+    List<Value> values = new List<Value>();
+    int maxPoint = -1;
+    List<Value> result;
+
+    public Solver(int N, int S, int T, int[] numbers)
+    {
+        this.N = N;
+        this.S = S;
+        this.T = T;
+        this.numbers = numbers;
+    }
 
     public List<Value> Solve(List<Value> numSoFar, int pointSoFar, int StepsRemain)
     {
@@ -30,7 +39,7 @@ class Solver
         return result;
     }
 
-    static void InnerSolve(List<Value> remaining, List<Value> numSoFar, int pointSoFar, int StepsRemain)
+    void InnerSolve(List<Value> remaining, List<Value> numSoFar, int pointSoFar, int StepsRemain)
     {
         System.Console.Out.WriteLine ("remaining   : {0}", string.Join(", ", remaining.Select(i => i.ToString())));
         System.Console.Out.WriteLine ("numSoFar    : {0}", string.Join(", ", numSoFar.Select(i => i.ToString())));
@@ -61,7 +70,7 @@ class Solver
         }
     }
 
-    static void CountInitValues()
+    void CountInitValues()
     {
         for (int i = 0; i < N - T + 1; i++)
         {
@@ -70,7 +79,7 @@ class Solver
         }
     }
 
-    static int CountValuesFrom(int startIndex)
+    int CountValuesFrom(int startIndex)
     {
         int newValue = 0;
         for (int j = 0; j < T; j++)
@@ -92,7 +101,7 @@ class Program
 
     static void Main(string[] args)
     {
-        var result = new Solver().Solve(new List<Value>(), 0, T);
+        var result = new Solver(N, S, T, numbers).Solve(new List<Value>(), 0, T);
         System.Console.Out.WriteLine ("The result is: {0}", string.Join(", ", result.Select(i => i.ToString())));
     }
 }
